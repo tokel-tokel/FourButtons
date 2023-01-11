6f03fd2ee0faa30280ea6c3c80b6ade9927900d0
tokel-tokel <ctepandstarik@gmail.com>
2023-01-11 18:57:49 +0300

    Add onClick for buttons

--- app/src/main/java/com/tokel/fourbuttons/activities/MainActivity.java
+++ app/src/main/java/com/tokel/fourbuttons/activities/MainActivity.java
@@ -3,7 +3,9 @@ package com.tokel.fourbuttons.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.tokel.fourbuttons.R;
 import com.tokel.fourbuttons.logic.*;
@@ -11,6 +13,7 @@ import com.tokel.fourbuttons.logic.*;
 public class MainActivity extends AppCompatActivity
 {
     private Button button1, button2, button3, button4;
+    private TextView textView;
     private ButtonHandler buttonHandler1, buttonHandler2, buttonHandler3, buttonHandler4;
 
     @Override
@@ -18,10 +21,27 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);
-        button4 = (Button) findViewById(R.id.button4);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        textView = findViewById(R.id.bottom_text);
         buttonHandler1 = new ButtonHandler1();
+    }
+    public void onClick1(View view)
+    {
+        textView.setText(buttonHandler1.getResponse());
+    }
+    public void onClick2(View view)
+    {
+        textView.setText(buttonHandler2.getResponse());
+    }
+    public void onClick3(View view)
+    {
+        textView.setText(buttonHandler3.getResponse());
+    }
+    public void onClick4(View view)
+    {
+        textView.setText(buttonHandler4.getResponse());
     }
 }
