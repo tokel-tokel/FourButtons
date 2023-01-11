9663131624aab15c7ede31b5a12957ef409b08a9
lil kaneki6ken <j32arnautov@gmail.com>
2023-01-11 19:52:53 +0300

    lil bro added

---
+++ app/src/main/java/com/tokel/fourbuttons/logic/ButtonHandler4.java
@@ -0,0 +1,8 @@
+package com.tokel.fourbuttons.logic;
+
+public class ButtonHandler4 implements ButtonHandler{
+    @Override
+    public String getResponse() {
+        return "aahahhaahhahahahah scull lil bro too fat" + "L";
+    }
+}
